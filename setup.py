d1ba570d6105014690e067b67c69e6727d54d590
Adam Haile <adammhaile@gmail.com>
2018-12-03 08:37:12 -0500

    Adding better version handling

--- setup.py
+++ setup.py
@@ -6,11 +6,18 @@ from setuptools import setup
 with open('./requirements.txt') as f:
     INSTALL_REQUIRES = f.read().splitlines()
 
+
+def _get_version():
+    from os.path import abspath, dirname, join
+    filename = join(dirname(abspath(__file__)), 'doozerlib', 'VERSION')
+    return open(filename).read().strip()
+
+
 setup(
     name="doozer",
     author="AOS ART Team",
     author_email="aos-team-art@redhat.com",
-    version="0.2.4",
+    version=_get_version(),
     description="CLI tool for managing and automating Red Hat software releases",
     url="https://github.com/openshift/doozer",
     license="Red Hat Internal",
