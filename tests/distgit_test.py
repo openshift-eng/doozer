81b0bce2a1ab8f617f589536fa1af32d50e6f86b
Thiago Alessio Pereira <talessio@redhat.com>
2019-05-24 11:09:34 +0200

    Cover pull_image behavior

--- tests/distgit_test.py
+++ tests/distgit_test.py
@@ -6,7 +6,7 @@ $ python -m doozerlib.distgit_test
 """
 import unittest
 import flexmock
-from mock import patch, ANY
+from mock import patch, ANY, call
 
 import StringIO
 import logging
@@ -109,6 +109,41 @@ class TestDistgitRecursiveOverwrite(unittest.TestCase):
         distgit.recursive_overwrite("my-source", "my-dest", {"ignore", "me"})
         cmd_assert_mock.assert_called_once_with(expected_cmd, retries=ANY)
 
+
+class TestDistgitPullImage(unittest.TestCase):
+    """
+    Mocking exectools.cmd_gather to prevent actual command executions.
+    Also mocking time.sleep for faster tests.
+    """
+
+    @patch("distgit.exectools.cmd_gather", return_value=(0, "", ""))
+    def test_generate_podman_command(self, cmd_gather_mock):
+        expected_cmd = ["podman", "pull", "my-image"]
+
+        distgit.pull_image("my-image")
+        cmd_gather_mock.assert_called_with(expected_cmd)
+
+    @patch("distgit.time.sleep", return_value=None)
+    @patch("distgit.exectools.cmd_gather", return_value=(1, "", ""))
+    def test_pull_fails_more_than_3_times(self, _, __):
+        expected_exception = distgit.exectools.RetryException
+        self.assertRaises(expected_exception, distgit.pull_image, "my-image")
+
+    @patch("distgit.logger.info", return_value=None)
+    @patch("distgit.time.sleep", return_value=None)
+    @patch("distgit.exectools.cmd_gather")
+    def test_custom_logging(self, cmd_gather_mock, _, logger_info_mock):
+        # pretending the cmd failed twice and succeeded on the third attempt.
+        cmd_gather_mock.side_effect = [(1, "", ""), (1, "", ""), (0, "", "")]
+
+        expected_logging_calls = [
+            call("Pulling image: my-image"),
+            call("Error pulling image my-image -- retrying in 60 seconds"),
+            call("Error pulling image my-image -- retrying in 60 seconds"),
+        ]
+
+        distgit.pull_image("my-image")
+        self.assertEqual(expected_logging_calls, logger_info_mock.mock_calls)
 
 
 class TestDistgit(unittest.TestCase):
