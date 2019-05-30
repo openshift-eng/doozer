de925d12da8f4c7ea2c7a449a2918619e3d743b5
Thiago Alessio Pereira <talessio@redhat.com>
2019-05-30 13:48:36 +0200

    Cover ImageDistGitRepo.push_image insecure

--- tests/distgit/image_repo/test_push_image.py
+++ tests/distgit/image_repo/test_push_image.py
@@ -60,3 +60,33 @@ brew-pulp-docker01.web.prod.ext.phx2.redhat.com:8888/origin-aggregated-logging:v
         ]
 
         self.assertEqual(expected_write_commands, mocked_bultin_open().write.mock_calls)
+
+    @patch("distgit.Dir")
+    @patch("distgit.os.mkdir")
+    @patch("distgit.open")
+    @patch("distgit.exectools.cmd_gather", return_value=(0, "stdout", "stderr"))
+    def test_insecure_oc_mirror_command(self, mocked_cmd_gather, *_):
+        repo = ImageDistGitRepo(Mock(**{
+            "config.push.late": False,
+            "runtime.group_config.insecure_source": True,
+
+            "get_latest_build_info.return_value": ("name", "version", "release"),
+            "get_default_push_names.return_value": [
+                "registry.reg-aws.openshift.com:443/openshift/logging-elasticsearch5",
+                "registry.reg-aws.openshift.com:443/openshift/ose-logging-elasticsearch5",
+            ],
+            "get_additional_push_names.return_value": [],
+            "runtime.group_config.urls.brew_image_host": "brew-pulp-docker01.web.prod.ext.phx2.redhat.com:8888",
+            "get_default_push_tags.return_value": [
+                "v4.1.0-201905191700",
+                "v4.1.0",
+            ],
+            "runtime.working_dir": "/my/working/dir",
+            "distgit_key": "logging-elasticsearch5", # is it correct ?
+            "config.name": "origin-aggregated-logging",
+        }), autoclone=False)
+
+        repo.push_image([], True)
+
+        expected_cmd = "oc image mirror  --insecure=true --filename=/my/working/dir/push/logging-elasticsearch5"
+        mocked_cmd_gather.assert_called_with(expected_cmd)
