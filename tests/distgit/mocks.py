cac01098f9b840de57986ca2d2de347decca7556
Thiago Aléssio <thiagoalessio@me.com>
2019-05-31 09:53:58 +0200

    Remove default value in mock
    The test setup should be more explicit, so that the assertions would immediately make sense to the reader, without the need to jump from the test to the mock definition every time.
    
    Co-Authored-By: Luke Meyer <sosiouxme@gmail.com>

--- tests/distgit/mocks.py
+++ tests/distgit/mocks.py
@@ -45,7 +45,7 @@ class MockMetadata(object):
         self.config = MockConfig()
         self.runtime = runtime
         self.logger = runtime.logger
-        self.name = "test"
+        self.name = None
         self.namespace = "namespace"
         self.distgit_key = "distgit_key"
 
