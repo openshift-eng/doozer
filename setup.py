9c7c24f314112d5cb72c263e6ae7c1ef72e6d535
Yuxiang Zhu <yuxzhu@redhat.com>
2019-11-11 21:26:16 +0800

    Fix doozerlib.cli is not included during installation
    In setup.py, call `find_packages` to auto-discover packages and use `exclude` parameter to exclude unit tests from installation.

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from setuptools import setup
+from setuptools import setup, find_packages
 
 with open('./requirements.txt') as f:
     INSTALL_REQUIRES = f.read().splitlines()
@@ -20,7 +20,7 @@ setup(
     long_description=open('README.md').read(),
     url="https://github.com/openshift/doozer",
     license="Apache License, Version 2.0",
-    packages=["doozerlib"],
+    packages=find_packages(exclude=["tests", "tests.*"]),
     include_package_data=True,
     entry_points={
         'console_scripts': [
@@ -28,6 +28,7 @@ setup(
         ]
     },
     install_requires=INSTALL_REQUIRES,
+    test_suite='tests',
     dependency_links=[],
     classifiers=[
         "Development Status :: 4 - Beta",
