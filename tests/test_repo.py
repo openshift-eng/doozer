97c04f8493449ea4d01593ed01ddfa5a8575c60f
Tim Bielawa <tbielawa@redhat.com>
2020-02-04 13:12:30 +0100

    Add more tests for repo class

--- tests/test_repo.py
+++ tests/test_repo.py
@@ -76,6 +76,39 @@ class TestRepos(unittest.TestCase):
             'optional': True,
         }
     }
+
+    # no conf
+    invalid_repo_config1 = {
+        'content_set': {
+            'default': 'rhel-7-server-ose-4.2-rpms',
+            'ppc64le': 'rhel-7-for-power-le-ose-4.2-rpms',
+            's390x': 'rhel-7-for-system-z-ose-4.2-rpms',
+            'optional': True,
+        }
+    }
+    # no content sets
+    invalid_repo_config2 = {
+        'conf': {
+            'enabled': 1,
+            'baseurl': {
+                'ppc64le': 'http://download-node-02.eng.bos.redhat.com/brewroot/repos/rhaos-4.4-rhel-8-build/latest/ppc64le/',
+                's390x': 'http://download-node-02.eng.bos.redhat.com/brewroot/repos/rhaos-4.4-rhel-8-build/latest/s390x/',
+                'x86_64': 'http://download-node-02.eng.bos.redhat.com/brewroot/repos/rhaos-4.4-rhel-8-build/latest/x86_64/',
+            }
+        }
+    }
+    # no baseurls
+    invalid_repo_config3 = {
+        'conf': {
+            'enabled': 1,
+        },
+        'content_set': {
+            'default': 'rhel-7-server-ose-4.2-rpms',
+            'ppc64le': 'rhel-7-for-power-le-ose-4.2-rpms',
+            's390x': 'rhel-7-for-system-z-ose-4.2-rpms',
+            'optional': True,
+        }
+    }
     arches = ['x86_64', 'ppc64le', 's390x']
     test_repo = 'rhaos-4.4-rhel-8-build'
 
@@ -98,3 +131,38 @@ class TestRepos(unittest.TestCase):
         conf_str_extra = self.repo_extras.conf_section('signed')
         # The repo with an 'extra_options' section is correct
         self.assertEqual(EXPECTED_EXTRA_OPTIONS_REPO, conf_str_extra)
+
+    def test_content_set(self):
+        """ensure content sets can be correctly selected"""
+        self.assertEqual("rhel-7-server-ose-4.2-rpms", self.repo.content_set('x86_64'))
+
+        with self.assertRaises(ValueError):
+            # Will not be a valid content set
+            self.repo.content_set('redhat')
+
+        # Add a fake content set to the 'invalid sets' list
+        self.repo.set_invalid_cs_arch('hatred')
+        # Now ensure there is an error when we try to use it
+        with self.assertRaises(ValueError):
+            self.repo.content_set('hatred')
+
+        # Manually indicate that a previously valid content set arch
+        # is no longer valid
+        self.repo.set_invalid_cs_arch('x86_64')
+        self.assertIsNone(self.repo.content_set('x86_64'))
+
+    def test_arches(self):
+        """ensure we can get the arches we configured the repo with"""
+        self.assertListEqual(self.repo.arches, self.arches)
+
+    def test_init_validation(self):
+        """ensure the init method checks for incorrectly configured repos"""
+        # no 'conf' section
+        with self.assertRaises(ValueError):
+            Repo('no-conf', self.invalid_repo_config1, self.arches)
+        # no 'content sets' section
+        with self.assertRaises(ValueError):
+            Repo('no-content-sets', self.invalid_repo_config2, self.arches)
+        # no 'base urls' section
+        with self.assertRaises(ValueError):
+            Repo('no-base-urls', self.invalid_repo_config3, self.arches)
