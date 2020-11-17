openshift-eng/doozer

commit 980051ef6fdbf66ba6774eaf78b16b71614be9d5

scan-sources speed-up

doozerlib/brew.py
@@ -20,7 +20,7 @@ import koji
 import koji_cli.lib
 
 from .model import Missing
-from .util import red_print
+from .util import red_print, total_size
 
 logger = logutil.getLogger(__name__)
 
@@ -204,42 +204,54 @@ latest_tag_change_events = {}
 cache_lock = Lock()
 
 
-def tags_changed_since_build(runtime, koji_client, build, tag_ids) -> Dict:
+def has_tag_changed_since_build(runtime, koji_client, build, tag, inherit=True) -> Dict:
     """
     :param runtime:  The doozer runtime
     :param koji_client: A koji ClientSession.
     :param build:  A build information dict returned from koji getBuild
-    :param tag_ids: A list of tag ids (or tag names) which should be assessed
-    :return: A map (potentially empty) with tagName -> changeEvent (where changeEvent is a map describing the event
-             and occurred after the build creation_event_id).
+    :param tag: A tag name or tag id which should be assessed.
+    :param inherit: If True, uses tag inheritance.
+    :return: None if the tag has not changed since build OR a change event dict for the build that was tagged
+            after the build argument was built.
     """
     build_nvr = build['nvr']
     build_event_id = build['creation_event_id']
 
     result = []
-    for tid in tag_ids:
-        with cache_lock:
-            latest_tag_change_event = latest_tag_change_events.get(tid, None)
+    with cache_lock:
+        latest_tag_change_event = latest_tag_change_events.get(tag, None)
 
-        if latest_tag_change_event is None:
-            # I don't like the use of this queryHistory call. It returns the most recently tagged (active=True)
-            # packages associated with a tag. This is a long list (e.g. 100s of packages for the
-            # rhaos candidate tag, so it might impact performance. What we want out of this is the
-            # very newest tagged package, but queryHistory does not seem to allow this (e.g. limit: 1).
-            # There is another API 'tagHistory' that can do this, but (1) it is supposed to be
-            # deprecated and (2) it does not allow event=# to constrain its search (required
-            # to support --brew-event). So we use this call. The first entry in the tag_listing
-            # field should be the most recently tagged package by this tag.
+    if latest_tag_change_event is None:
+        latest_tag_change_event = {}
+
+        # Note: There is an API 'tagHistory' that can do this, but (1) it is supposed to be
+        # deprecated and (2) it does not allow event=# to constrain its search (required
+        # to support --brew-event). So we use a combination of listTagged and queryHistory.
+
+        # The listTagged API will do much of this work for us. The reason is that it will report updates to
+        # a tag newest->oldest: https://pagure.io/koji/blob/fedf3ee9f9238ed74c34d51c5458a834732b3346/f/hub/kojihub.py#_1351
+        # In other words, listTagged('rhaos-4.7-rhel-8-build', latest=True, inherit=True)[0] describes a bulid that was
+        # most recently tagged into this tag (or inherited tags).
+        last_tagged_builds = koji_client.listTagged(tag, latest=True, inherit=inherit)
+        if last_tagged_builds:
+            last_tagged_build = last_tagged_builds[0]
+            # We now have the build that was tagged, but not WHEN it was tagged. It could have been built
+            # a long time ago, and recently tagged into the tag we care about. To figure this out, we
+            # need to query the tag_listing table.
+
+            found_in_tag_name = last_tagged_build['tag_name']  # If using inheritance, this can differ from tag
             # Example result of full queryHistory: https://gist.github.com/jupierce/943b845c07defe784522fd9fd76f4ab0
-            tag_listing = koji_client.queryHistory(table='tag_listing', tag=tid, active=True)['tag_listing']
-            latest_tag_change_event = {} if not tag_listing else tag_listing[0]
-            with cache_lock:
-                latest_tag_change_events[tid] = latest_tag_change_event
+            tag_listing = koji_client.queryHistory(table='tag_listing',
+                                                   tag=found_in_tag_name, build=last_tagged_build['build_id'])['tag_listing']
+            latest_tag_change_event = tag_listing[0]  # Order is by increasing age, so 0 is the most recent time this build was tagged
 
-        if latest_tag_change_event and latest_tag_change_event['create_event'] > build_event_id:
-            result[latest_tag_change_event['tag.name']] = latest_tag_change_event
+        with cache_lock:
+            latest_tag_change_events[tag] = latest_tag_change_event
 
-    runtime.logger.debug(f'Found that build of {build_nvr} (event={build_event_id}) occurred before tag changes: {result}')
+    if latest_tag_change_event and latest_tag_change_event['create_event'] > build_event_id:
+        runtime.logger.debug(f'Found that build of {build_nvr} (event={build_event_id}) occurred before tag changes: {latest_tag_change_event}')
+        return latest_tag_change_event
+
     return result
 
 
@@ -324,6 +336,16 @@ class KojiWrapper(koji.ClientSession):
         'newRepo',
     ])
 
+    # Methods which cannot be constrained, but are considered safe to allow even wthen brew-event is set.
+    # Why? If you know the parameters, those parameters should have already been constrained by another
+    # koji API call.
+    safe_methods = set([
+        'getEvent',
+        'getBuild',
+        'listArchives',
+        'listRPMs',
+    ])
+
     def __init__(self, koji_session_args, brew_event=None):
         """
         See class description on what this wrapper provides.
@@ -339,6 +361,11 @@ class KojiWrapper(koji.ClientSession):
     def clear_global_cache(cls):
         with cls.koji_wrapper_lock:
             cls.koji_wrapper_result_cache.clear()
+
+    @classmethod
+    def get_cache_size(cls):
+        with cls.koji_wrapper_lock:
+            return total_size(cls.koji_wrapper_result_cache)
 
     @classmethod
     def get_next_call_id(cls):
@@ -376,16 +403,23 @@ class KojiWrapper(koji.ClientSession):
         """
         brew_event = self.___brew_event
         if brew_event:
-            if method_name == 'queryHistory' and 'beforeEvent' not in kwargs:
-                kwargs = kwargs or {}
-                kwargs['beforeEvent'] = self.brew_event + 1
-            elif 'event' not in kwargs and method_name in KojiWrapper.methods_with_event:
-                kwargs = kwargs or {}
-                kwargs['event'] = brew_event
+            if method_name == 'queryHistory':
+                if 'beforeEvent' not in kwargs and 'before' not in kwargs:
+                    # Only set the kwarg if the caller didn't
+                    kwargs = kwargs or {}
+                    kwargs['beforeEvent'] = brew_event + 1
+            elif method_name in KojiWrapper.methods_with_event:
+                if 'event' not in kwargs:
+                    # Only set the kwarg if the caller didn't
+                    kwargs = kwargs or {}
+                    kwargs['event'] = brew_event
+            elif method_name in KojiWrapper.safe_methods:
+                # Let it go through
+                pass
             elif not kw_opts.brew_event_aware:
                 # If --brew-event has been specified and non-constrainable API call is invoked, raise
                 # an exception if the caller has not made clear that are ok with that via brew_event_aware option.
-                raise IOError('Non-constrainable koji api call with --brew-event set; you must use KojiWrapperOpts with brew_event_aware=True')
+                raise IOError(f'Non-constrainable koji api call ({method_name}) with --brew-event set; you must use KojiWrapperOpts with brew_event_aware=True')
 
         return kwargs
 
@@ -527,7 +561,3 @@ class KojiWrapper(koji.ClientSession):
                 retries -= 1
                 if retries == 0:
                     raise
-
-
-
-

doozerlib/cli/__init__.py
@@ -66,10 +66,12 @@ def print_version(ctx, param, value):
 @click.option('--debug', default=False, is_flag=True, help='Show debug output on console.')
 @click.option("--stream", metavar="STREAM_NAME PULLSPEC", nargs=2, multiple=True,
               help="Override a stream.yml entry with a specific pullspec.  [multiple]")
-@click.option("--upstream", "upstreams", metavar="DISTGIT_KEY COMMIT-ISH", multiple=True, nargs=2,
+@click.option("--lock-upstream", "upstreams", metavar="DISTGIT_KEY COMMIT-ISH", multiple=True, nargs=2,
               help="Override upstream source commits. [multiple]")
-@click.option("--downstream", "downstreams", metavar="DISTGIT_KEY COMMIT-ISH", multiple=True, nargs=2,
+@click.option("--lock-downstream", "downstreams", metavar="DISTGIT_KEY COMMIT-ISH", multiple=True, nargs=2,
               help="Checkout non-HEAD of distgit. This is primarily for testing and cannot be used to build images. [multiple]")
+@click.option("--lock-runtime-uuid", metavar="UUID", default=None, nargs=1,
+              help="Fixes the otherwise randomly determined run UUI")
 @click.option("--source", metavar="ALIAS PATH", nargs=2, multiple=True,
               help="Associate a path with a given source alias.  [multiple]")
 @click.option("--sources", metavar="YAML_PATH",

doozerlib/cli/scan_sources.py
@@ -55,37 +55,8 @@ def config_scan_source_changes(runtime, ci_kubeconfig, as_yaml):
 
     with runtime.shared_koji_client_session() as koji_api:
 
-        runtime.logger.info(f'Running scan with true latest koji/brew event: {koji_api.getLastEvent()}')
-        runtime.logger.info(f'Emulated override koji/brew event: {runtime.brew_event}')
-
-        # Different branches have different build tags & inheritances; cache the results to
-        # limit brew queries.
-        build_root_tag_id_cache = dict()
-
-        def get_build_root_inherited_tags(config_meta):
-            # Create a {map of tags => tag names} which contribute RPMs to a given build root
-            build_tag = config_meta.branch() + '-build'
-            if build_tag in build_root_tag_id_cache:
-                return build_root_tag_id_cache[build_tag]
-            build_tag_ids = [n['parent_id'] for n in koji_api.getFullInheritance(build_tag)]
-            final_build_tag_ids = {}
-            for bid in build_tag_ids:
-                tag_name = koji_api.getTag(bid)['name']
-                if tag_name.endswith(('-candidate', '-pending')):
-                    # We don't want our candidate tag to be taken in consideration. It will always
-                    # be changing as after any build.
-                    continue
-
-                # There are many tags in the buildroots that are unlikely to affect our builds.
-                # (e.g. rhel-7.8-z). Filter these down to those which reasonably affect us.
-                if any(('devtools' in tag_name,  # e.g. devtools-2019.4-rhel-7
-                        '-build' in tag_name,
-                        '-override' in tag_name)):
-                    final_build_tag_ids[bid] = tag_name
-
-            runtime.logger.info(f'For {config_meta.distgit_key}, found buildroot inheritance: {build_tag_ids}; Filtered down to {final_build_tag_ids}')
-            build_root_tag_id_cache[build_tag] = final_build_tag_ids
-            return final_build_tag_ids
+        runtime.logger.info(f'scan-sources coordinate: brew_event: {koji_api.getLastEvent(brew.KojiWrapperOpts(brew_event_aware=True))}')
+        runtime.logger.info(f'scan-sources coordinate: emulated_brew_event: {runtime.brew_event}')
 
         # First, scan for any upstream source code changes. If found, these are guaranteed rebuilds.
         for meta, change_info in runtime.scan_distgit_sources():
@@ -106,13 +77,11 @@ def config_scan_source_changes(runtime, ci_kubeconfig, as_yaml):
                             eldest_rpm_build = latest_rpm_build
 
                     # Detect if our buildroot changed since the oldest rpm of the latest build of the package was built.
-                    rpm_build_root_tag_ids = get_build_root_inherited_tags(meta).keys()
-                    build_root_changes = brew.tags_changed_since_build(runtime, koji_api, eldest_rpm_build, rpm_build_root_tag_ids)
+                    build_root_change = brew.has_tag_changed_since_build(runtime, koji_api, eldest_rpm_build, meta.build_root_tag(), inherit=True)
 
-                    if build_root_changes:
-                        changing_tag_names = build_root_changes.keys()
-                        reason = f'Latest package build was before buildroot changes: {changing_tag_names}'
-                        runtime.logger.info(f'{dgk} ({eldest_rpm_build}) in {package_name} is older than more recent buildroot change: {build_root_changes}')
+                    if build_root_change:
+                        reason = 'Oldest package rpm build was before buildroot change'
+                        runtime.logger.info(f'{dgk} ({eldest_rpm_build}) in {package_name} is older than more recent buildroot change: {build_root_change}')
                         needs_rebuild = True
 
                 if reason:
@@ -140,79 +109,90 @@ def config_scan_source_changes(runtime, ci_kubeconfig, as_yaml):
                 changing_image_metas.add(descendant_meta)
                 add_assessment_reason(descendant_meta, True, f'Ancestor {meta.distgit_key} is changing')
 
-        # To limit the size of the queries we are going to make, find the oldest image
-        eldest_image_event_ts = koji_api.getLastEvent()['ts']
+        # To limit the size of the queries we are going to make, find the oldest and newest image
+        oldest_image_event_ts = None
+        newest_image_event_ts = 0
         for image_meta in runtime.image_metas():
             info = image_meta.get_latest_build(default=None)
             if info is not None:
                 create_event_ts = koji_api.getEvent(info['creation_event_id'])['ts']
-                if create_event_ts < eldest_image_event_ts:
-                    eldest_image_event_ts = create_event_ts
+                if oldest_image_event_ts is None or create_event_ts < oldest_image_event_ts:
+                    oldest_image_event_ts = create_event_ts
+                if create_event_ts > newest_image_event_ts:
+                    newest_image_event_ts = create_event_ts
 
-        for image_meta in runtime.image_metas():
-            image_change, msg = image_meta.does_image_need_change(eldest_image_event_ts, changing_rpm_packages, get_build_root_inherited_tags(image_meta).keys())
-            if image_change:
-                add_image_meta_change(image_meta, msg)
+    runtime.logger.debug(f'Will be assessing tagging changes between newest_image_event_ts:{newest_image_event_ts} and oldest_image_event_ts:{oldest_image_event_ts}')
+    change_results = runtime.parallel_exec(
+        f=lambda image_meta, terminate_event: image_meta.does_image_need_change(changing_rpm_packages, image_meta.build_root_tag(), newest_image_event_ts, oldest_image_event_ts),
+        args=runtime.image_metas(),
+        n_threads=20
+    )
 
-        # does_image_name_change() checks whether its non-member builder images have changed
-        # but cannot determine whether member builder images have changed until anticipated
-        # changes have been calculated. The following loop does this. It uses while True,
-        # because technically, we could keep finding changes in intermediate builder images
-        # and have to ensure we pull in images that rely on them in the next iteration.
-        # fyi, changes in direct parent images should be detected by RPM changes, which
-        # does does_image_name_change will detect.
-        while True:
-            changing_image_dgks = [meta.distgit_key for meta in changing_image_metas]
-            for image_meta in all_image_metas:
-                dgk = image_meta.distgit_key
-                if dgk in changing_image_dgks:  # Already in? Don't look any further
-                    continue
+    for change_result in change_results.get():
+        meta, changing, msg = change_result
+        if changing:
+            add_image_meta_change(meta, msg)
 
-                for builder in image_meta.config['from'].builder:
-                    if builder.member and builder.member in changing_image_dgks:
-                        runtime.logger.info(f'{dgk} will be rebuilt due to change in builder member ')
-                        add_image_meta_change(image_meta, f'Builder group member has changed: {builder.member}')
-
-            if len(changing_image_metas) == len(changing_image_dgks):
-                # The for loop didn't find anything new, we can break
-                break
-
-        # We have our information. Now build the output report..
-        image_results = []
+    # does_image_name_change() checks whether its non-member builder images have changed
+    # but cannot determine whether member builder images have changed until anticipated
+    # changes have been calculated. The following loop does this. It uses while True,
+    # because technically, we could keep finding changes in intermediate builder images
+    # and have to ensure we pull in images that rely on them in the next iteration.
+    # fyi, changes in direct parent images should be detected by RPM changes, which
+    # does does_image_name_change will detect.
+    while True:
         changing_image_dgks = [meta.distgit_key for meta in changing_image_metas]
         for image_meta in all_image_metas:
             dgk = image_meta.distgit_key
-            is_changing = dgk in changing_image_dgks
-            image_results.append({
-                'name': dgk,
-                'changed': is_changing,
-                'reason': assessment_reason.get(f'{image_meta.qualified_key}+{is_changing}', 'No change detected'),
-            })
+            if dgk in changing_image_dgks:  # Already in? Don't look any further
+                continue
 
-        rpm_results = []
-        changing_rpm_dgks = [meta.distgit_key for meta in changing_rpm_metas]
-        for rpm_meta in all_rpm_metas:
-            dgk = rpm_meta.distgit_key
-            is_changing = dgk in changing_rpm_dgks
-            rpm_results.append({
-                'name': dgk,
-                'changed': is_changing,
-                'reason': assessment_reason.get(f'{rpm_meta.qualified_key}+{is_changing}', 'No change detected'),
-            })
+            for builder in image_meta.config['from'].builder:
+                if builder.member and builder.member in changing_image_dgks:
+                    runtime.logger.info(f'{dgk} will be rebuilt due to change in builder member ')
+                    add_image_meta_change(image_meta, f'Builder group member has changed: {builder.member}')
 
-        results = dict(
-            rpms=rpm_results,
-            images=image_results
-        )
+        if len(changing_image_metas) == len(changing_image_dgks):
+            # The for loop didn't find anything new, we can break
+            break
 
-        if ci_kubeconfig:  # we can determine m-os-c needs updating if we can look at imagestreams
-            results['rhcos'] = _detect_rhcos_status(runtime, ci_kubeconfig)
+    # We have our information. Now build the output report..
+    image_results = []
+    changing_image_dgks = [meta.distgit_key for meta in changing_image_metas]
+    for image_meta in all_image_metas:
+        dgk = image_meta.distgit_key
+        is_changing = dgk in changing_image_dgks
+        image_results.append({
+            'name': dgk,
+            'changed': is_changing,
+            'reason': assessment_reason.get(f'{image_meta.qualified_key}+{is_changing}', 'No change detected'),
+        })
 
-        if as_yaml:
-            click.echo('---')
-            click.echo(yaml.safe_dump(results, indent=4))
-            return
+    rpm_results = []
+    changing_rpm_dgks = [meta.distgit_key for meta in changing_rpm_metas]
+    for rpm_meta in all_rpm_metas:
+        dgk = rpm_meta.distgit_key
+        is_changing = dgk in changing_rpm_dgks
+        rpm_results.append({
+            'name': dgk,
+            'changed': is_changing,
+            'reason': assessment_reason.get(f'{rpm_meta.qualified_key}+{is_changing}', 'No change detected'),
+        })
 
+    results = dict(
+        rpms=rpm_results,
+        images=image_results
+    )
+
+    runtime.logger.debug(f'scan-sources coordinate: results:\n{yaml.safe_dump(results, indent=4)}')
+
+    if ci_kubeconfig:  # we can determine m-os-c needs updating if we can look at imagestreams
+        results['rhcos'] = _detect_rhcos_status(runtime, ci_kubeconfig)
+
+    if as_yaml:
+        click.echo('---')
+        click.echo(yaml.safe_dump(results, indent=4))
+    else:
         for kind, items in results.items():
             if not items:
                 continue
@@ -221,6 +201,8 @@ def config_scan_source_changes(runtime, ci_kubeconfig, as_yaml):
                 click.echo('  {} is {} (reason: {})'.format(item['name'],
                                                             'changed' if item['changed'] else 'the same',
                                                             item['reason']))
+
+    runtime.logger.info(f'KojiWrapper cache size: {int(brew.KojiWrapper.get_cache_size() / 1024)}KB')
 
 
 def _detect_rhcos_status(runtime, kubeconfig) -> list:

doozerlib/metadata.py
@@ -118,6 +118,9 @@ class Metadata(object):
             return self.config.distgit.branch
         return self.runtime.branch
 
+    def build_root_tag(self):
+        return '{}-build'.format(self.branch())
+
     def candidate_brew_tag(self):
         return '{}-candidate'.format(self.branch())
 
@@ -192,7 +195,7 @@ class Metadata(object):
         """
         Check whether the commit that we recorded in the distgit content (according to cgit)
         matches the commit of the source (according to git ls-remote) and has been built
-        (according to brew). Nothing is cloned and no existing clones are consulted.
+        (according to brew).
         Returns: (<bool>, message). If True, message describing the details is returned. If False,
                 None is returned.
         """

doozerlib/pushd.py
@@ -6,19 +6,9 @@ The Dir object is a context_manager that can be used with the Python 'with'
 clause.  The context manager idiom allows the user to execute some commands
 in a working directory other than the CWD and return without needing to
 explicitly handle it.
-
-Example:
-
-  # os.getcwd() returns /tmp/somewhere
-  with Dir("/tmp/somewhere/else"):
-      # os.getcwd() returns /tmp/somewhere/else
-      ....
-
-  # os.getcwd() returns /tmp/somewhere
 """
 from __future__ import absolute_import, print_function, unicode_literals
 
-import os
 import threading
 import pathlib
 
@@ -27,15 +17,13 @@ class Dir(object):
     """
     Context manager to handle directory changes safely.
 
-    On `__enter__`, `chdir`s to the given directory and on `__exit__`, `chdir`s
-    back to restore the previous `cwd`.
+    On `__enter__`, stores a pseudo current working directory. On __exit__
+    any previous Dir context's cwd is restored.
 
-    The current directory is also kept on thread-local storage and can be
-    accessed (e.g. by multi-threaded programs that cannot rely on `chdir`) via
-    the `getcwd` static method.
+    The current directory of the process (e.g. os.getcwd()) is never altered
+    as this would not be a threadsafe operation.
 
-    The `assert_exec` and `gather_exec` member functions use the directory in
-    effect automatically.
+    The exectools library honors these contexts automatically.
     """
     _tl = threading.local()
 
@@ -45,22 +33,21 @@ class Dir(object):
 
     def __enter__(self):
         self.previous_dir = self.getcwd()
-        os.chdir(self.dir)
         self._tl.cwd = self.dir
         return self.dir
 
     def __exit__(self, *args):
-        os.chdir(self.previous_dir)
         self._tl.cwd = self.previous_dir
 
     @classmethod
     def getcwd(cls):
         """
         Provide a context dependent current working directory. This method
-        will return the directory currently holding the lock.
+        will return the directory currently holding the lock. If not within
+        a Dir context, None will be returned.
         """
         if not hasattr(cls._tl, "cwd"):
-            cls._tl.cwd = os.getcwd()
+            return None
         return cls._tl.cwd
 
     @classmethod
@@ -69,4 +56,5 @@ class Dir(object):
         Provide a context dependent current working directory. This method
         will return a pathlib.Path for the directory currently holding the lock.
         """
-        return pathlib.Path(Dir.getcwd())
+        cwd = Dir.getcwd()
+        return None if not cwd else pathlib.Path(Dir.getcwd())

doozerlib/repos.py
@@ -3,6 +3,7 @@ from .model import Model, ModelException, Missing
 import yaml
 import requests
 import json
+import time
 
 DEFAULT_REPOTYPES = ['unsigned', 'signed']
 
@@ -315,7 +316,15 @@ class Repos(object):
             'Cache-Control': "no-cache"
         }
 
-        response = requests.request("POST", url, data=json.dumps(payload), headers=headers)
+        retry_count = 4
+        for i in range(retry_count):
+            try:
+                response = requests.request("POST", url, data=json.dumps(payload), headers=headers)
+                break
+            except:
+                if i == retry_count - 1:
+                    raise
+                time.sleep(5)
 
         resp_dict = response.json()
 

doozerlib/rpmcfg.py
@@ -1,16 +1,13 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, print_function, unicode_literals
 
-import glob
 import shutil
 import os
 import traceback
 import re
 import io
-import pathlib
-import json
-import logging
 import glob
+from typing import Optional
 
 from . import exectools
 from .pushd import Dir
@@ -41,9 +38,9 @@ remote_git_name = {name}
 
 class RPMMetadata(Metadata):
 
-    def __init__(self, runtime, data_obj, clone_source=True,
+    def __init__(self, runtime, data_obj, commitish: Optional[str] = None, clone_source=True,
                  source_modifier_factory=SourceModifierFactory()):
-        super(RPMMetadata, self).__init__('rpm', runtime, data_obj)
+        super(RPMMetadata, self).__init__('rpm', runtime, data_obj, commitish)
 
         self.source = self.config.content.source
         if self.source is Missing:

commit 0d9715928da6d15208206b1abb52a4c26baa440e

Fix Doozer crash with `'tuple' object does not support item assignment`
```
Traceback (most recent call last):
  File "./doozerlib/runtime.py", line 98, in wrapper
    return func(*args, **kwargs)
  File "./doozerlib/runtime.py", line 110, in wrapper
    return func(*args)
  File "./doozerlib/cli/__main__.py", line 180, in <lambda>
    lambda rpm, terminate_event: rpm.build_rpm(
  File "./doozerlib/rpmcfg.py", line 486, in build_rpm
    self.assert_golang_versions()
  File "./doozerlib/rpmcfg.py", line 544, in assert_golang_versions
    latest_builds = brew.get_latest_builds([(buildroot, component) for component in golang_components], "rpm", None, brew_session)
  File "./doozerlib/brew.py", line 184, in get_latest_builds
    for tag, component_name in tag_component_tuples:
  File "/home/dev/.local/lib/python3.8/site-packages/koji/__init__.py", line 3215, in __exit__
    self.call_all()
  File "/home/dev/.local/lib/python3.8/site-packages/koji/__init__.py", line 3187, in call_all
    _results = self._session._callMethod('multiCall', args, {})
  File "./doozerlib/brew.py", line 564, in _callMethod
    params[-1] = self.modify_koji_call_kwargs(method_name, possible_kwargs, aggregate_kw_opts)
TypeError: 'tuple' object does not support item assignment

```

This issue was introduced by
https://github.com/openshift/doozer/pull/311 to speed up scan-sources.

Simply converting the tuple to list should fix it.

doozerlib/brew.py
@@ -487,6 +487,7 @@ class KojiWrapper(koji.ClientSession):
                 method_name = call_dict['methodName']
                 params = self.modify_koji_call_params(method_name, call_dict['params'], aggregate_kw_opts)
                 if params:
+                    params = list(params)
                     # Assess whether we need to inject event of beforeEvent into the koji call kwargs
                     possible_kwargs = params[-1]  # last element could be normal arg or kwargs dict
                     if isinstance(possible_kwargs, dict) and possible_kwargs.get('__starstar', None):