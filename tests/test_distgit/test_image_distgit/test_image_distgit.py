1bf3bc487991b2709b69d752c70628ecc0a166ea
Yuxiang Zhu <yuxzhu@redhat.com>
2021-06-17 13:18:56 +0000

    Fix unit tests

--- tests/test_distgit/test_image_distgit/test_image_distgit.py
+++ tests/test_distgit/test_image_distgit/test_image_distgit.py
@@ -335,12 +335,12 @@ FROM some-base-image:some-tag AS builder
 # __doozer=yum-update
 USER 0
 # __doozer=yum-update
-RUN yum update -y && yum clean all
+RUN yum update -y && yum clean all  # set final_stage_user in ART metadata if this fails
 LABEL name=value
 RUN some-command
 FROM another-base-image:some-tag
 # __doozer=yum-update
-RUN yum update -y && yum clean all
+RUN yum update -y && yum clean all  # set final_stage_user in ART metadata if this fails
 COPY --from=builder /some/path/a /some/path/b
         """.strip().splitlines()
         self.assertListEqual(actual, expected)
@@ -372,14 +372,14 @@ FROM some-base-image:some-tag AS builder
 # __doozer=yum-update
 USER 0
 # __doozer=yum-update
-RUN yum update -y && yum clean all
+RUN yum update -y && yum clean all  # set final_stage_user in ART metadata if this fails
 LABEL name=value
 RUN some-command
 FROM another-base-image:some-tag
 # __doozer=yum-update
 USER 0
 # __doozer=yum-update
-RUN yum update -y && yum clean all
+RUN yum update -y && yum clean all  # set final_stage_user in ART metadata if this fails
 # __doozer=yum-update
 USER 1002
 COPY --from=builder /some/path/a /some/path/b
@@ -404,14 +404,14 @@ FROM some-base-image:some-tag AS builder
 # __doozer=yum-update
 USER 0
 # __doozer=yum-update
-RUN yum update -y && yum clean all
+RUN yum update -y && yum clean all  # set final_stage_user in ART metadata if this fails
 LABEL name=value
 RUN some-command
 FROM another-base-image:some-tag
 # __doozer=yum-update
 USER 0
 # __doozer=yum-update
-RUN yum update -y && yum clean all
+RUN yum update -y && yum clean all  # set final_stage_user in ART metadata if this fails
 # __doozer=yum-update
 USER 1001
 COPY --from=builder /some/path/a /some/path/b
@@ -444,14 +444,14 @@ FROM some-base-image:some-tag AS builder
 # __doozer=yum-update
 USER 0
 # __doozer=yum-update
-RUN yum update -y && yum clean all
+RUN yum update -y && yum clean all  # set final_stage_user in ART metadata if this fails
 LABEL name=value
 RUN some-command
 FROM another-base-image:some-tag
 # __doozer=yum-update
 USER 0
 # __doozer=yum-update
-RUN yum update -y && yum clean all
+RUN yum update -y && yum clean all  # set final_stage_user in ART metadata if this fails
 # __doozer=yum-update
 USER 1001
 COPY --from=builder /some/path/a /some/path/b
