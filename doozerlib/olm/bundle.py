7216ef73c2a647483a406be34a72a5e182cbe394
Siddharth Sharma <sidsharm@redhat.com>
2021-09-27 16:54:47 -0400

    initial commit

--- doozerlib/olm/bundle.py
+++ doozerlib/olm/bundle.py
@@ -561,8 +561,8 @@ class OLMBundle(object):
     @property
     def operator_framework_tags(self):
         return {
-            'operators.operatorframework.io.bundle.channel.default.v1': self.channel,
-            'operators.operatorframework.io.bundle.channels.v1': self.channel,
+            'operators.operatorframework.io.bundle.channel.default.v1': 'stable',
+            'operators.operatorframework.io.bundle.channels.v1': f'{self.channel},stable',
             'operators.operatorframework.io.bundle.manifests.v1': 'manifests/',
             'operators.operatorframework.io.bundle.mediatype.v1': 'registry+v1',
             'operators.operatorframework.io.bundle.metadata.v1': 'metadata/',
