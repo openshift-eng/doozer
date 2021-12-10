60eeb449fd685dca93ae3a50a83b85f3562c6914
Siddharth Sharma <sidsharm@redhat.com>
2021-12-10 10:54:54 -0500

    fix lint

--- doozerlib/cli/inspect_rhcos.py
+++ doozerlib/cli/inspect_rhcos.py
@@ -5,6 +5,7 @@ from doozerlib.cli import cli
 from doozerlib.util import brew_arches
 from doozerlib.cli.release_gen_payload import PayloadGenerator
 
+
 @cli.command("inspect:rhcos", short_help="Inspect latest rhcos builds for consistency")
 @click.pass_obj
 def inspect_rhcos(runtime):
@@ -23,7 +24,7 @@ def inspect_rhcos(runtime):
             raise IOError(f"No RHCOS latest found for {version} / {arch}")
         rhcos_builds.append(RHCOSBuildInspector(runtime, pullspec, arch))
     logger.info(f"Checking following builds for inconsistency: {rhcos_builds}")
-    rhcos_inconsistencies: Dict[str, List[str]] = PayloadGenerator.find_rhcos_build_rpm_inconsistencies(rhcos_builds)
+    rhcos_inconsistencies = PayloadGenerator.find_rhcos_build_rpm_inconsistencies(rhcos_builds)
     if rhcos_inconsistencies:
         print(f'Found RHCOS inconsistencies in builds {rhcos_builds}: {rhcos_inconsistencies}')
         exit(1)
