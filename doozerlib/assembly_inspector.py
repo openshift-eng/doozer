0eb513cf205b8a0f9ad76ea4f74fb594dfd0f8c3
Siddharth Sharma <sidsharm@redhat.com>
2021-12-15 18:14:43 -0500

    support for permits

--- doozerlib/assembly_inspector.py
+++ doozerlib/assembly_inspector.py
@@ -6,16 +6,16 @@ from kobo.rpmlib import parse_nvr
 from doozerlib import util, Runtime
 from doozerlib.image import BrewBuildImageInspector
 from doozerlib.rpmcfg import RPMMetadata
-from doozerlib.assembly import assembly_rhcos_config, AssemblyTypes, assembly_permits, AssemblyIssue, AssemblyIssueCode, assembly_type
+from doozerlib.assembly import assembly_rhcos_config, AssemblyTypes, Assembly_permits, AssemblyIssue, AssemblyIssueCode, assembly_type
 from doozerlib.rhcos import RHCOSBuildInspector, RHCOSBuildFinder
 
 
 class AssemblyInspector:
 
-    def __init__(self, runtime: Runtime, brew_session: ClientSession):
+    def __init__(self, runtime: Runtime, brew_session: ClientSession = None, lite: bool = False):
         self.runtime = runtime
         self.brew_session = brew_session
-        if runtime.mode != 'both':
+        if not lite and runtime.mode != 'both':
             raise ValueError('Runtime must be initialized with "both"')
 
         self.assembly_rhcos_config = assembly_rhcos_config(self.runtime.releases_config, self.runtime.assembly)
@@ -23,6 +23,8 @@ class AssemblyInspector:
         self._rpm_build_cache: Dict[int, Dict[str, Optional[Dict]]] = {}  # Dict[rhel_ver] -> Dict[distgit_key] -> Optional[BuildDict]
         self._permits = assembly_permits(self.runtime.releases_config, self.runtime.assembly)
 
+        if lite:
+            return
         # If an image component has a latest build, an ImageInspector associated with the image.
         self._release_image_inspectors: Dict[str, Optional[BrewBuildImageInspector]] = dict()
         for image_meta in runtime.get_for_release_image_metas():
--- doozerlib/cli/inspect_stream.py
+++ doozerlib/cli/inspect_stream.py
@@ -5,23 +5,32 @@ from doozerlib.cli import cli
 from doozerlib.util import brew_arches
 from doozerlib.cli.release_gen_payload import PayloadGenerator
 from doozerlib.assembly import AssemblyIssueCode
+from doozerlib.assembly import AssemblyIssue
+from doozerlib.assembly_inspector import AssemblyInspector
 
 
 @cli.command("inspect:stream", short_help="Inspect stream assembly for assembly issues")
 @click.argument("code", type=click.Choice([code.name for code in AssemblyIssueCode]), required=True)
 @click.pass_obj
 def inspect_stream(runtime, code):
-    runtime.initialize(clone_distgits=False)
-    logger = runtime.logger
+    code = AssemblyIssueCode[code]
     if runtime.assembly != 'stream':
-        logger.warning(f'Disregarding non-stream assembly: {runtime.assembly}. This command is only intended for '
+        print(f'Disregarding non-stream assembly: {runtime.assembly}. This command is only intended for '
                        f'stream')
+    runtime.assembly = 'stream'
+    runtime.initialize(clone_distgits=False)
+    assembly_inspector = AssemblyInspector(runtime, lite=True)
 
-
-    if code == AssemblyIssueCode.INCONSISTENT_RHCOS_RPMS.name:
-        rhcos_builds, rhcos_inconsistencies = _check_inconsistent_rhcos_rpms(runtime)
+    if code == AssemblyIssueCode.INCONSISTENT_RHCOS_RPMS:
+        rhcos_builds, rhcos_inconsistencies = _check_inconsistent_rhcos_rpms(runtime, assembly_inspector)
         if rhcos_inconsistencies:
-            print(f'Found RHCOS inconsistencies in builds {rhcos_builds}: {rhcos_inconsistencies}')
+            msg = f'Found RHCOS inconsistencies in builds {rhcos_builds}: {rhcos_inconsistencies}'
+            print(msg)
+            assembly_issue = AssemblyIssue(msg, component='rhcos', code=code)
+            if assembly_inspector.does_permit(assembly_issue):
+                print(f'Assembly permits code {code}.')
+                exit(0)
+            print(f'Assembly does not permit code {code}. Failed')
             exit(1)
         print(f'RHCOS builds consistent {rhcos_builds}')
         exit(0)
@@ -29,20 +38,17 @@ def inspect_stream(runtime, code):
         print(f'AssemblyIssueCode {code} not supported at this time :(')
         exit(1)
 
-def _check_inconsistent_rhcos_rpms(runtime):
+def _check_inconsistent_rhcos_rpms(runtime, assembly_inspector):
     logger = runtime.logger
     major = runtime.group_config.vars.MAJOR
     minor = runtime.group_config.vars.MINOR
-    version = f'{major}.{minor}'
     not_arm = major == 4 and minor < 9
     rhcos_builds = []
     for arch in brew_arches:
         if not_arm and arch == 'aarch64':
             continue
-        build_id, pullspec = RHCOSBuildFinder(runtime, version, arch, False).latest_machine_os_content()
-        if not pullspec:
-            raise IOError(f"No RHCOS latest found for {version} / {arch}")
-        rhcos_builds.append(RHCOSBuildInspector(runtime, pullspec, arch))
+        build_inspector = assembly_inspector.get_rhcos_build(arch)
+        rhcos_builds.append(build_inspector)
     logger.info(f"Checking following builds for inconsistency: {rhcos_builds}")
     rhcos_inconsistencies = PayloadGenerator.find_rhcos_build_rpm_inconsistencies(rhcos_builds)
     return rhcos_builds, rhcos_inconsistencies
